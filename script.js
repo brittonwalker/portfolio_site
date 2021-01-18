413ff83aee20eefa331fa36c77fdeab2a6c14e1b
Britton Walker <bwalker1801@gmail.com>
2021-01-17 23:11:27 -0500

    Test again again

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var BW = {
     init: function(){
       this.smoothScroll(this.els.link);
-      console.log('welcome');
+      console.log('welcome dude');
     },
     els: {
       link: $('a[href^="#"]')
