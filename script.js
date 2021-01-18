66628c58d49686985e1c0a31bfd83f11d82982be
Britton Walker <bwalker1801@gmail.com>
2021-01-17 23:13:57 -0500

    Test this thang

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var BW = {
     init: function(){
       this.smoothScroll(this.els.link);
-      console.log('welcome dude');
+      console.log('welcome dude');  
     },
     els: {
       link: $('a[href^="#"]')
