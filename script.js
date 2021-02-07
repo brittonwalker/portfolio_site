c1df5d2602c9e68249f4eb1c1a4d8fd7cedf6b26
Britton Walker <bwalker1801@gmail.com>
2021-02-06 19:45:45 -0500

    Removing

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   var BW = {
     init: function(){
       this.smoothScroll(this.els.link);
-      console.log('welcome dude hell yeah change');  
     },
     els: {
       link: $('a[href^="#"]')
