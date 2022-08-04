bda6475b1ed433ce154652ce4b7b44dc01625a7d
Britton Walker <bwalker1801@gmail.com>
2022-08-04 10:44:46 -0400

    Using tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#131313',
+        primary: 'rgb(245, 238, 230)',
+        hightlight: '#ED1C24',
+      },
+    },
+  },
+  plugins: [],
+};
